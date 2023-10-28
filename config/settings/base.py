369f191deb8134a5a30c8a88a69e3f2e9654c6c1
Matvey <matveykawork@gmail.com>
2023-10-28 16:54:42 +0300

    feat: User model & manager

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,6 +11,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'apps.users.apps.UsersConfig'
 ]
 
 MIDDLEWARE = [
@@ -81,3 +83,5 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
